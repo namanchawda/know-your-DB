8c6c821f40e4b90bb6dc1c7a7acb1a56dc9f2043
Naman Chawda <92266293+namanchawda@users.noreply.github.com>
2026-01-11 12:48:02 -0800

    Update api.ts

--- db-query-frontend/lib/api.ts
+++ db-query-frontend/lib/api.ts
@@ -1,4 +1,6 @@
-const BASE_URL = 'https://know-your-db.onrender.com';
+const BASE_URL =
+  process.env.NEXT_PUBLIC_API_URL!;
+
 
 function getConnectionId() {
   return localStorage.getItem('connectionId');
